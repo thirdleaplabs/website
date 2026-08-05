4c9c6a25d2f1194f565cff21a5f75e57faf66df8
Gokul Kartha <kartha.gokul@gmail.com>
2026-08-05 07:13:37 +0200

    Align portfolio around VisionX, WhaleScanner and GoPaySol

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ import { OpenSource } from './pages/OpenSource';
 import { Company } from './pages/Company';
 import { Contact } from './pages/Contact';
 import { WhaleScanner } from './pages/WhaleScanner';
+import { GoPaySol } from './pages/GoPaySol';
 import { VisionX } from './pages/VisionX';
-import { FlameTrax } from './pages/FlameTrax';
 import { LegalDisclaimer } from './pages/LegalDisclaimer';
 import { PrivacyPolicy } from './pages/PrivacyPolicy';
 import { SecurityNotice } from './pages/SecurityNotice';
@@ -22,8 +22,8 @@ function App() {
       <Route path="/company" element={<Company />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/work/whalescanner" element={<WhaleScanner />} />
+      <Route path="/work/gopaysol" element={<GoPaySol />} />
       <Route path="/work/visionx" element={<VisionX />} />
-      <Route path="/work/flametrax" element={<FlameTrax />} />
       <Route path="/legal-disclaimer" element={<LegalDisclaimer />} />
       <Route path="/privacy-policy" element={<PrivacyPolicy />} />
       <Route path="/security-notice" element={<SecurityNotice />} />
@@ -31,10 +31,10 @@ function App() {
       <Route path="/solutions" element={<Navigate to="/capabilities" replace />} />
       <Route path="/products" element={<Navigate to="/work" replace />} />
       <Route path="/labs/whalescanner" element={<Navigate to="/work/whalescanner" replace />} />
+      <Route path="/labs/gopaysol" element={<Navigate to="/work/gopaysol" replace />} />
       <Route path="/labs/visionx" element={<Navigate to="/work/visionx" replace />} />
-      <Route path="/labs/flametrax" element={<Navigate to="/work/flametrax" replace />} />
-      <Route path="/labs/gopaysol" element={<Navigate to="/work" replace />} />
       <Route path="/products/whalescanner" element={<Navigate to="/work/whalescanner" replace />} />
+      <Route path="/products/gopaysol" element={<Navigate to="/work/gopaysol" replace />} />
       <Route path="/about" element={<Navigate to="/company" replace />} />
       <Route path="/services" element={<Navigate to="/capabilities" replace />} />
       <Route path="*" element={<Navigate to="/" replace />} />
